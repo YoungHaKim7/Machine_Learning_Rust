9bd73072c9515c5fe51b398ac52eceba6c1b83c7
GYoung <ytok1108@aol.com>
2024-11-21 14:06:55 +0900

    matrix dot.method 드디어 구현완료

--- 003_Practical_Machine_learning_Rust/03_std_array_matrix/a01_matrix_multi/README.md
+++ 003_Practical_Machine_learning_Rust/03_std_array_matrix/a01_matrix_multi/README.md
@@ -1,20 +1,18 @@
 # Result
 
 ```bash
+Matrix 2D:
 [1, 2, 3]
 [4, 5, 6]
 [7, 8, 9]
-[7, 8, 9]
 
+Matrix Mul:
 [1]
 [2]
 [3]
 
-
-# Result
-
+Result:
 [14]
 [32]
-[50]
 [50]
 ```
--- 003_Practical_Machine_learning_Rust/03_std_array_matrix/a01_matrix_multi/src/main.rs
+++ 003_Practical_Machine_learning_Rust/03_std_array_matrix/a01_matrix_multi/src/main.rs
@@ -1,12 +1,10 @@
-fn matrix_multiply(matrix: [[i32; 3]; 3], mul: [[i32; 0]; 3]) -> [[i32; 0]; 3] {
-    let mut result = [[0; 0]; 3];
+fn matrix_multiply(matrix: [[i32; 3]; 3], mul: [[i32; 1]; 3]) -> [[i32; 1]; 3] {
+    let mut result = [[0; 1]; 3];
 
     for i in 0..3 {
-        for j in 0..3 {
-            let one_row =
-                matrix[i][j] * mul[i][0] + matrix[0][j] * mul[i][0] + matrix[0][j] * mul[i][0];
+        for j in 0..1 {
             for k in 0..3 {
-                result[k][0] += one_row;
+                result[i][j] += matrix[i][k] * mul[k][j];
             }
         }
     }
@@ -15,29 +13,23 @@ fn matrix_multiply(matrix: [[i32; 3]; 3], mul: [[i32; 0]; 3]) -> [[i32; 0]; 3] {
 }
 
 fn main() {
-    let mut matrix_2d = [[0; 3]; 3];
-    let mut mat_mul = [[1; 1]; 3];
+    let matrix_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let mat_mul = [[1], [2], [3]];
 
-    matrix_2d[0][0] = 1;
-    matrix_2d[0][1] = 2;
-    matrix_2d[0][2] = 3;
-    matrix_2d[1][0] = 4;
-    matrix_2d[1][1] = 5;
-    matrix_2d[1][2] = 6;
-    matrix_2d[2][0] = 7;
-    matrix_2d[2][1] = 8;
-    matrix_2d[2][2] = 9;
-    mat_mul[1][0] = 2;
-    mat_mul[2][0] = 3;
+    let mat_result = matrix_multiply(matrix_2d, mat_mul);
 
-    mat_mul = matrix_multiply(matrix_2d, mat_mul);
-
-    for mat_2d in matrix_2d {
-        println!("{mat_2d:?}");
+    println!("Matrix 2D:");
+    for row in matrix_2d {
+        println!("{:?}", row);
     }
-    println!();
-    for mul in mat_mul {
-        println!("{mul:?}");
+
+    println!("\nMatrix Mul:");
+    for row in mat_mul {
+        println!("{:?}", row);
     }
-    println!();
+
+    println!("\nResult:");
+    for row in mat_result {
+        println!("{:?}", row);
+    }
 }
